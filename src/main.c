58ce3b352beb008c6896264cfa89f74a39e491ea
chancodea07 <58924133+chancodea07@users.noreply.github.com>
2021-01-20 16:45:19 +0800

    补充例子说明

--- src/main.c
+++ src/main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include "../include/a.h" //注意这里的路径引用
 int main(int argc, char const *argv[])
 {
-    printf("Hello World\n");
+    greet();
     return 0;
 }
 
+void greet()
+{
+    printf("Hello World\n");
+}
